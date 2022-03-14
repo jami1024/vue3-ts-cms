556a045e8df344717d6c5e3f19139f981b48b25f
jijianming <996013797@qq.com>
2022-03-14 12:50:34 +0800

    项目基础配置

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path  = require('path')
+
+module.exports = {
+  // 1.配置方式1:CLI提供的属性
+  outputDir: './build',
+  configureWebpack: (config) => {
+    config.resolve.alias = {
+      '@': path.resolve(__dirname, 'src'),
+      components: '@/components'
+    }
+  }
+}
