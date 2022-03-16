008fbdfde0da1ed18e08f01760e135d9b436155a
jijianming <996013797@qq.com>
2022-03-16 13:21:39 +0800

    局部引用element-plus,拆分单独组建

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { createApp } from 'vue'
+import { registerApp } from './global'
 import App from './App.vue'
 
-import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
+// 全局引用
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+// import 'element-plus/lib/theme-chalk/base.css'
 
 import router from './router'
 import store from './store'
 const app = createApp(App)
+registerApp(app)
 app.use(router)
 app.use(store)
-app.use(ElementPlus)
+// app.use(ElementPlus)
 app.mount('#app')
