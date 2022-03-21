7662e651685b79334af609aff885bd6ecf7a7324
jijianming <996013797@qq.com>
2022-03-21 16:42:31 +0800

    axios封装拦截器

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,9 +1,19 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
-
+import type { ZLRequestInterceptors, ZLRequestConfig } from './type'
 class ZLRequest {
   instance: AxiosInstance
-  constructor(config: AxiosRequestConfig) {
+  interceptors?: ZLRequestInterceptors
+  constructor(config: ZLRequestConfig) {
     this.instance = axios.create(config)
+    this.interceptors = config.interceptors
+    this.instance.interceptors.request.use(
+      this.interceptors?.requestInterceptor,
+      this.interceptors?.requestInterceptorCatch
+    )
+    this.instance.interceptors.response.use(
+      this.interceptors?.responseInterceptor,
+      this.interceptors?.responseInterceptorCatch
+    )
   }
   request(config: AxiosRequestConfig): void {
     this.instance.request(config).then((res) => {
