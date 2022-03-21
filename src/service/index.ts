234299c4c54e6c783d3b9a9d761082ca0b0b4544
jijianming <996013797@qq.com>
2022-03-21 13:06:06 +0800

    axios封装逻辑

---
+++ src/service/index.ts
@@ -0,0 +1,5 @@
+// service统一出口
+import ZLRequest from './request'
+const zlRequest = new ZLRequest()
+
+export default zlRequest
---
+++ src/service/request/index.ts
@@ -0,0 +1,5 @@
+class ZLRequest {
+  request() {}
+  get() {}
+}
+export default ZLRequest
