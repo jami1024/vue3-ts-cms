f4a68b68c8a94504c40e6c5e096709e13b76325a
jijianming <996013797@qq.com>
2022-04-18 14:13:46 +0800

    优化分页bug

--- src/components/page-content/src/page-content.vue
+++ src/components/page-content/src/page-content.vue
@@ -92,7 +92,7 @@ export default defineComponent({
     const isQuery = usePermission(props.pageName, 'query')
 
     // 1.双向绑定pageInfo
-    const pageInfo = ref({ currentPage: 0, pageSize: 10 })
+    const pageInfo = ref({ currentPage: 1, pageSize: 10 })
 
     watch(pageInfo, () => getPageData())
     // 2.发送网络请求
@@ -101,7 +101,7 @@ export default defineComponent({
       store.dispatch('system/getPageListAction', {
         pageName: props.pageName,
         queryInfo: {
-          offset: pageInfo.value.currentPage * pageInfo.value.pageSize,
+          offset: (pageInfo.value.currentPage - 1) * pageInfo.value.pageSize,
           size: pageInfo.value.pageSize,
           ...queryInfo
         }
